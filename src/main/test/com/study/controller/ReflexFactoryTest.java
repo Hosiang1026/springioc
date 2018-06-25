8e1ae6c5cd04a9430cf8a98c16d41b934df3de98
Howe Hsiang <hosiang969@sina.com>
2018-06-25 10:47:15 +0800

    Test SpringIOC

--- src/main/test/com/study/controller/ReflexFactoryTest.java
+++ src/main/test/com/study/controller/ReflexFactoryTest.java
@@ -3,6 +3,11 @@ package com.study.controller;
 import com.study.service.Fruit;
 import org.junit.Test;
 
+/**
+ * 测试反射+简单工厂模式
+ *
+ * @author Howe Hsiang
+ */
 public class ReflexFactoryTest extends BaseTest {
 
     @Test
